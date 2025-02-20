e36f2dd26367438691c05e4b39ed55d45e7414a1
DeliaML <deliamartinezlinares@gmail.com>
2025-02-20 13:15:12 +0100

    Objetos

---
+++ src/Tema4/Ciclomotor.java
@@ -0,0 +1,28 @@
+package Tema4;
+
+public class Ciclomotor extends Vehiculo {
+
+    private int cc;
+
+    public Ciclomotor() {
+        this.cc = 100;
+    }
+
+    public Ciclomotor(int cc) {
+        this.cc = cc;
+    }
+
+    public int getCc() {
+        return cc;
+    }
+
+    public void setCc(int cc) {
+        if (cc > 0) this.cc = cc;
+    }
+
+    public static boolean needsCarnet(int cc) {
+
+        if (cc >= 125) return true;
+        return false;
+    }
+}
---
+++ src/Tema4/Perro.java
@@ -0,0 +1,49 @@
+package Tema4;
+
+public class Perro extends Animal {
+
+    private int hambre;
+    private int sueño;
+
+    public int comer(int hambre, int comida) {
+
+        if (comida > hambre + 20) return -20;
+        return hambre - comida;
+    }
+
+    public int dormir(int sueño, int siesta) {
+
+        if (siesta > sueño) return 0;
+        return sueño - siesta;
+    }
+
+    public void hacerRuido() {
+        System.out.println("Guau");
+    }
+
+    public Perro() {
+        this.hambre = 0;
+        this.sueño = 0;
+    }
+
+    public Perro(int hambre, int sueño) {
+        this.hambre = hambre;
+        this.sueño = sueño;
+    }
+
+    public int getHambre() {
+        return hambre;
+    }
+
+    public int getSueño() {
+        return sueño;
+    }
+
+    public void setHambre(int hambre) {
+        if (hambre >= -20) this.hambre = hambre;
+    }
+
+    public void setSueño(int sueño) {
+        if (sueño >= 0) this.sueño = sueño;
+    }
+}
